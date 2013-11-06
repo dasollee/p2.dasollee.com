b1267799302cc2e56d06d7e60c8279affe03a886
Dasol Lee <amdcpybt@gmail.com>
2013-11-07 04:33:47 +0900

    final

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -35,6 +35,7 @@ class users_controller extends base_controller {
     # For now, just confirm they've signed up - 
     # You should eventually make a proper View for this
     echo 'You\'re signed up';
+    echo "<a href='/posts'> Back to Posts</a>";
         
     }
 
